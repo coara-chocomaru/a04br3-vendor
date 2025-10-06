927be02d780ea2df1e3f2694f072066af8314f27
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-10-06 19:22:52 +0900

    Add files via upload

---
+++ Android.mk
@@ -0,0 +1,52 @@
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),a04br3)
+
+include  
+LOCAL_MODULE := MTKLogger 
+LOCAL_MODULE_OWNER := a04br3
+LOCAL_SRC_FILES := proprietary/vendor/app/MTKLogger/MTKLogger.apk 
+LOCAL_CERTIFICATE := platform 
+LOCAL_MODULE_TAGS := optional 
+LOCAL_MODULE_CLASS := APPS 
+LOCAL_DEX_PREOPT := false 
+LOCAL_MODULE_SUFFIX := .apk 
+LOCAL_VENDOR_MODULE := true 
+include 
+
+include  
+LOCAL_MODULE := MTKThermalManager 
+LOCAL_MODULE_OWNER := a04br3
+LOCAL_SRC_FILES := proprietary/vendor/app/MTKThermalManager/MTKThermalManager.apk 
+LOCAL_CERTIFICATE := platform 
+LOCAL_MODULE_TAGS := optional 
+LOCAL_MODULE_CLASS := APPS 
+LOCAL_DEX_PREOPT := false 
+LOCAL_MODULE_SUFFIX := .apk 
+LOCAL_VENDOR_MODULE := true 
+include 
+
+include  
+LOCAL_MODULE := CustomPropInterface 
+LOCAL_MODULE_OWNER := a04br3
+LOCAL_SRC_FILES := proprietary/vendor/framework/CustomPropInterface.jar 
+LOCAL_CERTIFICATE := platform 
+LOCAL_MODULE_TAGS := optional 
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES 
+LOCAL_MODULE_SUFFIX := .jar 
+LOCAL_VENDOR_MODULE := true 
+include 
+
+include  
+LOCAL_MODULE := mediatek-res 
+LOCAL_MODULE_OWNER := a04br3
+LOCAL_SRC_FILES := proprietary/vendor/framework/mediatek-res/mediatek-res.apk 
+LOCAL_CERTIFICATE := platform 
+LOCAL_MODULE_TAGS := optional 
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES 
+LOCAL_MODULE_SUFFIX := .apk 
+LOCAL_VENDOR_MODULE := true 
+include 
+
+endif
+
---
+++ a04br3-vendor.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, vendor/sts/a04br3/a04br3-vendor-blobs.mk)
