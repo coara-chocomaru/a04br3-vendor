6c7225e14bea49ad2be6dd03c2c31596bbc8cfb3
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-10-24 15:27:25 +0900

    Update a04br3-vendor-blobs.mk

--- a04br3-vendor-blobs.mk
+++ a04br3-vendor-blobs.mk
@@ -46,7 +46,6 @@ PRODUCT_COPY_FILES += \
     vendor/sts/a04br3/proprietary/etc/media_codecs_mediatek_video.xml:system/etc/media_codecs_mediatek_video.xml \
     vendor/sts/a04br3/proprietary/etc/permissions/platform.xml:system/etc/permissions/platform.xml \
     vendor/sts/a04br3/proprietary/etc/permissions/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
-    vendor/sts/a04br3/proprietary/etc/permissions/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     vendor/sts/a04br3/proprietary/etc/permissions/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
     vendor/sts/a04br3/proprietary/etc/permissions/android.hardware.microphone.xml:system/etc/permissions/android.hardware.microphone.xml \
     vendor/sts/a04br3/proprietary/etc/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
