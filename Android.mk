1a847cc53a24b63bc85e189ee4a4e0c90366207d
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-13 08:48:39 +0900

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -11,6 +11,37 @@ LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := custom_package_filter
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/dummy
+LOCAL_SRC_FILES := proprietary/empty
+LOCAL_OVERRIDES_PACKAGES := \
+    Calendar \
+    Camera2 \
+    DeskClock \
+    Email \
+    ExactCalculator \
+    Calculator \
+    Jelly \
+    LatinIME \
+    LockClock \
+    WallpaperPicker \
+    AudioFX \
+    Contacts \
+    Eleven \
+    Gallery2 \
+    Recorder \
+    Trebuchet \
+    Messaging \
+    Exchange2 \
+    BlockedNumberProvider \
+    Dialer \
+    SimToolkit \
+    Stk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := NexusLauncherIcons
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := proprietary/app/NexusLauncherIcons/NexusLauncherIcons.apk
